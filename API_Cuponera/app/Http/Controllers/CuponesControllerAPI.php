9a7b5eb70ef65ad3c57da7a5cf9c6cbf59a1d60b
Marco Gerardo Serrano Lopez <marcolopez121@outlook.com>
2023-05-06 17:24:12 -0600

    Primer Ingreso de datos

---
+++ API_Cuponera/app/Http/Controllers/CuponesControllerAPI.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CuponesControllerAPI extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $cupones=DB::table('cupones')->get();
+        return $cupones;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $cupon = $user = DB::table('cupones')->where('ID_Cupon',$id)->first();
+        return $cupon;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cuponera=['ID_Cupon'=>$id,'Estado_Cupon'=>$request->Estado_Cupon];
+        return DB::update('UPDATE cupones SET Estado_Cupon=:Estado_Cupon WHERE ID_Cupon=:ID_Cupon', $cuponera);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
